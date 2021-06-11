61a9f81c757c5db252fcebc90a8efea791390948
Rom <romainprevostdev@gmail.com>
2021-06-11 15:56:32 +0200

    cleaning composannt racine et composant App sans commentaires

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,9 @@
-// == Import npm
 import React from 'react';
 
-// == Import
 import './styles.scss';
 
-// == Composant
 const App = () => (
   <div className="app" />
 );
 
-// == Export
 export default App;
