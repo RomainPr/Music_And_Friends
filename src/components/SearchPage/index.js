d1a34eabaded5a1abfa74e742bdfb991eeb3426e
Litinerance <l.saulnier@outlook.com>
2021-06-19 20:09:22 +0200

    add property musicians,bands,places to SearchPage

--- src/components/SearchPage/index.js
+++ src/components/SearchPage/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+
+
+
+
+
+
 //import containers
 import CheckBox from 'src/containers/CheckBox';
 import CitiesField from 'src/containers/CitiesField';
 import InstrumentsField from 'src/containers/InstrumentsField';
 import StylesField from 'src/containers/StylesField';
+
 
 import Button from '@material-ui/core/Button';
 import Input from '@material-ui/core/Input';
@@ -14,14 +21,28 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import ListItemText from '@material-ui/core/ListItemText';
 
+//M UI
 import Select from '@material-ui/core/Select';
 import Checkbox from '@material-ui/core/Checkbox';
 import ArrowDropDownCircleIcon from '@material-ui/icons/ArrowDropDownCircle';
+import Slider from 'react-slick';
+import PropTypes from 'prop-types';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import Avatar from '@material-ui/core/Avatar';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import StarIcon from '@material-ui/icons/Star';
 
 import './style.scss';
 import { RestoreOutlined } from '@material-ui/icons';
 
 export default function SearchPage({
+  musicians, bands, places,
   categories, 
   categoryName, 
   onChangeCategoryValue,
@@ -29,6 +50,17 @@ export default function SearchPage({
   searchResult}) {
   
   console.log(searchResult)
+
+  const fullList = [...musicians, ...bands, ...places];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    adaptiveHeight: true,
+  };
 
   return (
     <div>
@@ -73,7 +105,50 @@ export default function SearchPage({
         </div>
 
       </form>
-      
+      <div className="profilsCards">
+      <Container maxWidth="lg">
+        <h2 className="profilsCards__title">Les derniers profils</h2>
+        <Slider {...settings}>
+          {fullList.map((item) => (
+            <Grid item xs={4} md={12}>
+              <Card id="card">
+                <div id="card__image">
+                  <CardMedia
+                    component="img"
+                    alt="BandBackground"
+                    title="BandBackground"
+                    image="https://images.unsplash.com/photo-1517147177326-b37599372b73"
+                  />
+                  <Avatar
+                    id="card__avatar"
+                    src="https://i.pravatar.cc/300"
+                  />
+                  <IconButton
+                    id="card__favorite"
+                  >
+                    <StarIcon />
+                  </IconButton>
+                </div>
+                <CardContent id="card__content">
+                  <h2 className="bandName">{item.name}<span>{item.band_name}</span><span>{item.pseudo}</span></h2>
+                  <h4 className="localization">{item.city}</h4>
+                  <p className="description">{item.description}</p>
+                </CardContent>
+                <CardActions id="card__footer">
+                  <Button
+                    variant="contained"
+                    size="large"
+                    color="primary"
+                  >
+                    Voir le profil
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Slider>
+      </Container>
+    </div>
       {/* <ul className="container-card-result">
       {searchResult.map((name) => (
         //VOIR COMPONENT CARD ?
@@ -91,6 +166,9 @@ export default function SearchPage({
 }
 
 SearchPage.propTypes = {
+  musicians: PropTypes.array.isRequired,
+  bands: PropTypes.array.isRequired,
+  places: PropTypes.array.isRequired,
   categories:PropTypes.array.isRequired,
   onChangeCategoryValue: PropTypes.func.isRequired,
   onClickSearch:PropTypes.func.isRequired,
