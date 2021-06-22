4ae91502e23366a418d4c68491d468f86366dfc0
Rom <romainprevostdev@gmail.com>
2021-06-22 16:27:30 +0200

    maj

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,32 +1,44 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 
 import Nav from 'src/containers/connectedNav';
 
 import background from 'src/assets/HeaderBg.jpg';
 import './styles.scss';
 
-const Header = () => (
-  <div className="header" style={{ backgroundImage: `url(${background})` }}>
-    <div className="header__nav">
-      <Nav />
-    </div>
-    <div className="header__middle">
-      <h1>Trouvez votre prochain "Groupe/Musicien/Evènement"</h1>
-      <div className="header__middle__search">
-        <Button
-          component={Link}
-          to="/search"
-          variant="contained"
-          size="large"
-          color="primary"
-        >
-          Rechercher
-        </Button>
+const useStyles = makeStyles(() => ({
+  searchButton: {
+    background: '#FDA13D',
+  },
+}));
+
+function Header() {
+  const classes = useStyles();
+
+  return (
+    <div className="header" style={{ backgroundImage: `url(${background})` }}>
+      <div className="header__nav">
+        <Nav />
+      </div>
+      <div className="header__middle">
+        <h1>Trouvez votre prochain "Groupe/Musicien/Evènement"</h1>
+        <div className="header__middle__search">
+          <Button
+            className={classes.searchButton}
+            component={Link}
+            to="/search"
+            variant="contained"
+            size="large"
+            color="primary"
+          >
+            Rechercher
+          </Button>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+}
 
 export default Header;
